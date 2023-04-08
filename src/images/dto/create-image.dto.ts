fa3bd5d2859dcd7ebf6c5f385dc0b9455b19a310
WebToLaure <lmarmus@hotmail.fr>
2023-04-08 13:30:13 +0200

    feat: image DTO

---
+++ src/images/dto/create-image.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty, IsEmail,} from "class-validator";
+
+
+export class CreateImageDto {
+
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    fieldName: string;
+}
