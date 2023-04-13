30a8d913e7553b44168880478c705a6bf2fe5e1c
WebToLaure <lmarmus@hotmail.fr>
2023-04-13 09:23:18 +0200

    fix:image controller

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFiles, UseInterceptors, UseGuards, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFiles, UseInterceptors, UseGuards, UploadedFile, Res } from '@nestjs/common';
 import { Express } from 'express';
 import { ImagesService } from './images.service';
 import { fileFilter } from '../config';
@@ -20,18 +20,19 @@ export class ImagesController {
   @ApiOperation({ summary: "Ajout d'un fichier " })
   @ApiResponse({ status: 201, description: 'Fichier téléchargé' })
 
-  @UseInterceptors(FileInterceptor('file',{
+  @UseInterceptors(FileInterceptor('file', {
     storage: diskStorage({  //fonction utilisable avec multer retourne un moteur de stockage implémenté pour stocker les photos en local.
       destination: './uploads',
       filename: (req, file, callback) => {
-        const uniqueSuffix = Date.now() +'-'+ Math.round(Math.random() * 1e9); //retourne un numéro multer propre à chaque photo
-        const ext = extname(file.originalname); //le nom du fichier originel, uploadé par l'utilisateur
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9); //retourne un numéro multer propre à chaque photo
+        const ext = extname(file.originalname); //extension du fichier original
         const filename = `${uniqueSuffix}${ext}`;
         callback(null, filename);
-      } 
+      }
     }),
-  })) 
-    
+    fileFilter: fileFilter,
+  }))
+
   async uploadFile(@UploadedFile() file: Express.Multer.File) { //@Body()createImageDto :CreateImageDto,
     console.log(file);
 
@@ -41,6 +42,13 @@ export class ImagesController {
       message: "Votre photo a bien été ajoutée"
     };
   }
+
+
+  @Get(':imgpath')
+  seeUploadedFile(@Param('imgpath') image, @Res() res) {
+    return res.sendFile(image, { root: './uploads' });
+  }
+
 
   @Get()
   findAll() {
